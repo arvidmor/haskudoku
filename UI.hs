0c1430c16f79586b69137e8462c3659e2edc1539
Arvid Morelid <arvid.morelid@hotmail.com>
2022-02-17 17:05:15 +0100

    Defined attributes for locked and input cells. Fixed some import statements with clashing names

--- UI.hs
+++ UI.hs
@@ -1,11 +1,12 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module UI where
 import Dat
+import qualified Dat
 
-import Graphics.Vty
+import Graphics.Vty hiding (Input)
 import qualified Graphics.Vty
 
-import Brick
+import Brick hiding (Up, Down)
 import Brick.Widgets.Core
 import Brick.Widgets.Center
 import Brick.Widgets.Border
@@ -15,16 +16,21 @@ import qualified Brick as Brick.Types
 import Data.Matrix
 import Data.List.Split (chunksOf)
 import Data.Char (digitToInt)
+import Prelude hiding (Right, Left)
 
 mkGame :: Game
-mkGame = Game {
+mkGame = insert (Input 6) (1, 2) $ insert (Lock 5) (1,1) Game {
     grid = newSudokuMatrix ,
     focusedCell = (5, 5),
     complete = False
 }
 
-generalAttr = attrName "general"
-attributes = attrMap defAttr [(generalAttr, bg blue)]
+lockAttr    = attrName "Lock"
+inputAttr   = attrName "Input"
+attributes = attrMap defAttr [
+      (lockAttr, white `on` black)
+    , (inputAttr, blue `on` black)
+    ]
 
 app :: App Game a Name
 app = App { appDraw         = drawGame
@@ -39,25 +45,25 @@ app = App { appDraw         = drawGame
     Event handling inspired by Evan Relf: https://github.com/evanrelf/sudoku-tui.git
 -}
 
-handleEvent :: Game -> BrickEvent Name a-> EventM Name (Next Game)
+handleEvent :: Game -> BrickEvent Name a -> EventM Name (Next Game)
 --Navigation
 handleEvent g (VtyEvent (EvKey key [])) =
     case key of 
     --Navigation
-    KUp         -> continue $ step Dat.Up g
-    KDown       -> continue $ step Dat.Down g
-    KLeft       -> continue $ step Dat.Left  g
-    KRight      -> continue $ step Dat.Right  g
+    KUp         -> continue $ step Up g
+    KDown       -> continue $ step Down g
+    KLeft       -> continue $ step Left  g
+    KRight      -> continue $ step Right  g
     --Input numbers
-    (KChar '1') -> continue $ insert (Dat.Input 1) (focusedCell g) g
-    (KChar '2') -> continue $ insert (Dat.Input 2) (focusedCell g) g
-    (KChar '3') -> continue $ insert (Dat.Input 3) (focusedCell g) g
-    (KChar '4') -> continue $ insert (Dat.Input 4) (focusedCell g) g
-    (KChar '5') -> continue $ insert (Dat.Input 5) (focusedCell g) g
-    (KChar '6') -> continue $ insert (Dat.Input 6) (focusedCell g) g
-    (KChar '7') -> continue $ insert (Dat.Input 7) (focusedCell g) g
-    (KChar '8') -> continue $ insert (Dat.Input 8) (focusedCell g) g
-    (KChar '9') -> continue $ insert (Dat.Input 9) (focusedCell g) g
+    (KChar '1') -> continue $ insert (Input 1) (focusedCell g) g
+    (KChar '2') -> continue $ insert (Input 2) (focusedCell g) g
+    (KChar '3') -> continue $ insert (Input 3) (focusedCell g) g
+    (KChar '4') -> continue $ insert (Input 4) (focusedCell g) g
+    (KChar '5') -> continue $ insert (Input 5) (focusedCell g) g
+    (KChar '6') -> continue $ insert (Input 6) (focusedCell g) g
+    (KChar '7') -> continue $ insert (Input 7) (focusedCell g) g
+    (KChar '8') -> continue $ insert (Input 8) (focusedCell g) g
+    (KChar '9') -> continue $ insert (Input 9) (focusedCell g) g
      --Global events
     (KChar 'q') -> halt g
     _           -> continue g
@@ -77,9 +83,9 @@ drawCell cell = withBorderStyle unicode
     $ hLimitPercent 10  
     $ vLimitPercent 10 
     $ case cell of
-    Lock x      ->  str $ show x
-    Dat.Input x ->  str $ show x
-    Empty       ->  str " E "
+    Lock x      ->  withAttr lockAttr  $ str $ show x
+    Input x     ->  withAttr inputAttr $ str $ show x
+    Empty       ->  str "  "
 
 --List all cell widgets in chunks of 9
 drawGrid :: Game -> [[Widget Name]]
@@ -100,3 +106,6 @@ drawHelp = withBorderStyle unicodeBold
     $ borderWithLabel (str "Help")
     $ vLimitPercent 50 
     $ str "Navigate: \n ↑ ↓ ← →" <=> str "Exit: q"
+
+drawTest :: Game -> Widget Name
+drawTest g = undefined --withBorderStyle unicodeRounded 
