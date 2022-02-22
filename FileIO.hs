5f573f755f0d0d5c775879a56cb021fe56ea1d71
Arvid Morelid <arvid.morelid@hotmail.com>
2022-02-22 15:08:03 +0100

    Added IO file to handle read and write actions

---
+++ FileIO.hs
@@ -0,0 +1,21 @@
+module FileIO where
+
+import Dat
+import UI
+import Data.Matrix (fromLists, toLists)
+import System.IO
+
+
+--File IO by Evan Relf, with some refactoring for our implementation of the grid
+
+importGrid :: String -> Game
+importGrid = (\g -> Game (fromLists g) (5, 5) False) . read
+
+exportGrid :: Game -> String
+exportGrid game = show (toLists (grid game))
+
+saveGrid :: Game -> FilePath -> IO ()
+saveGrid game filename = writeFile filename (exportGrid game)
+
+loadGrid :: FilePath -> IO Game
+loadGrid filename = importGrid <$> readFile filename
