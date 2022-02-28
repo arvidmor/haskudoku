d2214cbcced5cafeab9d080871c5535baeaa8882
ida <ida.s.hellqvist@gmail.com>
2022-02-28 10:34:04 +0100

    Added function specifications

--- Types.hs
+++ Types.hs
@@ -3,30 +3,49 @@ module Types where
 import Data.Matrix
 
 {- The value of a given cell in a sudoku-grid
-Lock x represents a number in the grid that is predefined, and thus immutable,
-Input x represents a number in the grid that was input by the player.
+Lock x c represents a number in the grid at the coordinates c that is predefined, and thus immutable.
+Input x c represents a number in the grid at the coordinates c that is input by the player.
+Note x c represents a list of numbers in the grid at the coordinates c.  
 Empty is an empty cell
-    INVARIANT:  0 < x <= 10 where x == (Input x) || (Lock x)
+    INVARIANT:  0 < x < 10 where x == (Input x c) || (Lock x c) (Note??)
 -}
 data Cell = Lock Int Coord    |
             Input Int Coord   |
             Note [Int] Coord  |
             Empty Coord      deriving (Eq, Show, Read) --Cell has a locked value (predefined), input value (input by user) or is empty
 
-type Grid       = Matrix Cell --Matrix of cell values
+{- A matrix containing all the cells of a sudoku puzzle
+A grid is a matrix where each element is a cell.
+    INVARIANT: The size of a grid is a 9 x 9 matrix.
+-}
+type Grid = Matrix Cell
 
-type Coord      = (Int, Int)    --(Row index, Column index)
+{- The coordinates of a cell in a grid
+In the coordinates (x, y), x represents the row number and y represents the column number.
+    INVARIANT: 0 < x < 10   &&    0 < y < 10
+-}
+type Coord = (Int, Int)    --(Row index, Column index)
 
-{- Direction type to be used for navigating the grid
-
+{- Indicating direction when navigating the grid.
+Up represents moving upwards in the grid.
+Down represents moving downwards in the grid.
+Left represents movin to the left in the grid.
+Right represents moving to the right in the grid.
 -}
 data Direction  = Up | Down | Left | Right  deriving Show
 
---The game state passed to defaultMain 
+{- State of the current game.
+grid represents the grid.
+focusedCell is the coordinates of the cell that the player is currently at.
+complete is a puzzle complete flag.
+-}
 data Game = Game {
-    grid        :: Grid,        --The sudoku grid
-    focusedCell :: Coord,       --Cell currently in focus of cursor
-    complete    :: Bool         --Puzzle complete flag
+    grid        :: Grid,
+    focusedCell :: Coord,
+    complete    :: Bool
 } deriving Show
 
+{- ... description of what the data type represents ... 
+ ... description of how the datatype represents data ...
+-}
 type Name = ()
