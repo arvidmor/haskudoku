a5fb670b8823458a74c0cb620cdfde3af3be1865
Arvid Morelid <arvid.morelid@hotmail.com>
2022-03-01 12:17:13 +0100

    Made tests for some grid functions

--- Tests.hs
+++ Tests.hs
@@ -6,4 +6,12 @@ import Main
 import Solver
 import Types
 import UI
+import Test.HUnit
 import Test.QuickCheck
+
+--Grid functions
+--Main Solver functions
+--Testcase to make sure that stepping R/L and U/D are inverse operations no matter where in the grid 
+test1 = TestCase $ assertEqual "step" (emptyGame {focusedCell = (1, 1)}) (step Types.Down $ step Types.Up $ step Types.Left $ step Types.Right (emptyGame {focusedCell = (1, 1)}))
+
+test2 = TestCase $ assertEqual "toggleNote" emptyGame (toggleNote 1 (5, 5) $ toggleNote 1 (5, 5) emptyGame)
