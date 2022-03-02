35fb6994a46a49d97ebc2d486d8b0ae777c56e11
ida <ida.s.hellqvist@gmail.com>
2022-03-02 16:43:28 +0100

    Added test for legal row

--- Tests.hs
+++ Tests.hs
@@ -16,8 +16,12 @@ test1 = TestCase $ assertEqual "step" (emptyGame {focusedCell = (1, 1)}) (step T
 --Toggling a note on and off gives an empty cell again
 test2 = TestCase $ assertEqual "toggleNote" (grid emptyGame) (grid (toggleNote 1 (5, 5) $ toggleNote 1 (5, 5) emptyGame))
 
+--Illegal to add a number to a subgrid where that number is already present 
 test3 = TestCase $ assertEqual "legalSubgrid" False (legalInSubGrid (Input 1 (5,5)) (listSubGrid (getCoordFromCell (Input 1 (5,5)))) (insert (Input 1 (4,5)) emptyGame))
 
-tests = TestList [test1, test2, test3]
+--Illegal to add a number to a row where that number is already present 
+test4 = TestCase $ assertEqual "legalRow" False (legalInRow (Input 1 (5,5)) (insert (Input 1 (5,4)) emptyGame))
+
+tests = TestList [test1, test2, test3, test4]
 
 performTests = runTestTT tests
